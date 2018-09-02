fedcbcdf8d12bd896008b56048d27e62b89e85f1
Vattanan <vattanan.bu@gmail.com>
2018-09-02 21:19:23 +0700

    refatoring

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,49 +1,49 @@
 import { expect } from 'chai'
 import withResolve from '../dist'
 
-const SUCCESS_RESPONSE = 'SUCCESS_RESPONSE'
-const ERROR_RESPONSE = 'ERROR_RESPONSE'
+const RESPONSE_SUCCESS = 'RESPONSE_SUCCESS'
+const RESPONSE_ERROR = 'RESPONSE_ERROR'
 
 describe('withResolve', () => {
   it('withResolve can use with function (without error)', async () => {
-    const fnSuccess = () => SUCCESS_RESPONSE
+    const fnSuccess = () => RESPONSE_SUCCESS
     const [error, result] = await withResolve(fnSuccess)()
 
     expect(error).to.be.null
-    expect(result).to.equal(SUCCESS_RESPONSE)
+    expect(result).to.equal(RESPONSE_SUCCESS)
   })
 
   it('withResolve can use with function (with error)', async () => {
-    const fnFail = () => { throw new Error(ERROR_RESPONSE) }
+    const fnFail = () => { throw new Error(RESPONSE_ERROR) }
     const [error, result] = await withResolve(fnFail)()
 
-    expect(error.message).to.equal(ERROR_RESPONSE)
+    expect(error.message).to.equal(RESPONSE_ERROR)
     expect(result).to.be.undefined
   })
 
   it('withResolve can use with promise function (without error)', async () => {
     const fnPromiseSuccess = () => new Promise((resolve) => {
-      resolve(SUCCESS_RESPONSE)
+      resolve(RESPONSE_SUCCESS)
     })
     const [error, result] = await withResolve(fnPromiseSuccess)()
 
     expect(error).to.be.null
-    expect(result).to.equal(SUCCESS_RESPONSE)
+    expect(result).to.equal(RESPONSE_SUCCESS)
   })
 
   it('withResolve can use with promise function (with error)', async () => {
     const fnPromiseFail = () => new Promise((_, reject) => {
-      reject(new Error(ERROR_RESPONSE))
+      reject(new Error(RESPONSE_ERROR))
     })
     const [error, result] = await withResolve(fnPromiseFail)()
 
-    expect(error.message).to.equal(ERROR_RESPONSE)
+    expect(error.message).to.equal(RESPONSE_ERROR)
     expect(result).to.be.undefined
   })
 
   it('withResolve must throw error when first parameter is not function', async () => {
     try {
-      withResolve(SUCCESS_RESPONSE)()
+      withResolve(RESPONSE_SUCCESS)()
       expect.fail()
     } catch (error) {
       expect(error.message).to.equal('first parameter must be function.')
