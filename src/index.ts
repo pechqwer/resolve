ed524bcbb7446e7904a13586bbf98ed66156272b
Vattanan <vattanan.bu@gmail.com>
2018-09-02 16:44:06 +0700

    add src

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const withResolve = (func: (...params: any[]) => any) => (...params: any[]) => {
+  if (!(func instanceof Function)) throw new Error('first parameter must be function.')
+
+  return new Promise(async (resolve) => {
+    try {
+      const result = await func(...params)
+      resolve([null, result])
+    } catch (error) {
+      resolve([error])
+    }
+  })
+}
+
+export default withResolve
