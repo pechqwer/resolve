cedc2c9092bf5164a7562576a688590278be0435
Vattanan Burapugdee <vattanan.b@cdg.co.th>
2018-09-04 01:53:51 +0700

    refacoring

--- dist/wrapperWithPromiseAll.js
+++ dist/wrapperWithPromiseAll.js
@@ -9,14 +9,14 @@ const validateParams = (limit, params) => {
     }
 };
 exports.default = (funcs, params) => {
-    const trigger = {
-        countSuccess: 0,
-        error: [],
-        result: [],
-    };
     return new Promise((resolve) => {
         if (params.length > 0)
             validateParams(funcs.length, params);
+        const trigger = {
+            countSuccess: 0,
+            error: [],
+            result: [],
+        };
         const maxTrigger = funcs.length;
         const done = (i, error, result = null) => {
             trigger.countSuccess += 1;
