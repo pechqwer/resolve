756c8e390dc40b99c129f3e28adee1257cffea92
Vattanan Burapugdee <vattanan.b@cdg.co.th>
2018-11-20 16:54:46 +0700

    implement function for support promise instance

---
+++ src/helper/validation.ts
@@ -0,0 +1,3 @@
+export const isFuntion = (value: any): boolean => value instanceof Function
+
+export const isPromise = (value: any): boolean => value instanceof Promise
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
+import { isFuntion, isPromise } from './helper/validation'
 import wrapperPromise from './wrapperPromise'
 import wrapperPromiseAll from './wrapperPromiseAll'
 
-const validationFunc = (funcs: Array<(...params: any[]) => any>) => {
-  if (!(funcs instanceof Array)) throw new Error('funcs must be array.')
-  if (funcs.length === 0) throw new Error('funcs must not empty.')
-  if (!funcs.every(f => f instanceof Function)) throw new Error('element in funcs must be function.')
+const validationFunc = (actions: Array<(...params: any[]) => any>) => {
+  if (!(actions instanceof Array)) throw new Error('actions must be array.')
+  if (actions.length === 0) throw new Error('actions must not empty.')
+  // TODO: support Promise instance and function
+  if (!actions.every(f => isFuntion(f) || isPromise(f) )) {
+    throw new Error('element in actions must be function or promise instance.')
+  }
 }
 
-export default function(...funcs: Array<(...params: any[]) => any>) {
+export default function(...actions: Array<(...params: any[]) => any>) {
   return (...params: any[]): Promise<[any, any]> | Promise<[any[], any[]]> => {
-    validationFunc(funcs)
+    validationFunc(actions)
 
-    if (funcs.length === 1) return wrapperPromise(funcs[0], params)
+    if (actions.length === 1) return wrapperPromise(actions[0], params)
 
-    return wrapperPromiseAll(funcs, params)
+    return wrapperPromiseAll(actions, params)
   }
 }
