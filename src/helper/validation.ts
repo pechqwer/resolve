7c755388480994b2f2d48df66005997074caba63
Vattanan Burapugdee <vattanan.b@cdg.co.th>
2018-11-28 14:11:21 +0700

    support params is undefined in multi resolve

--- src/helper/validation.ts
+++ src/helper/validation.ts
@@ -1,3 +1,3 @@
 export const isFuntion = (value: any): boolean => value instanceof Function
 
-export const isPromise = (value: any): boolean => value instanceof Promise
+export const isPromiseInstance = (value: any): boolean => value instanceof Promise
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import { isFuntion, isPromise } from './helper/validation'
+import { isFuntion, isPromiseInstance } from './helper/validation'
 import wrapperPromise from './wrapperPromise'
 import wrapperPromiseAll from './wrapperPromiseAll'
 
 const validationFunc = (actions: Array<(...params: any[]) => any>) => {
   if (!(actions instanceof Array)) throw new Error('actions must be array.')
   if (actions.length === 0) throw new Error('actions must not empty.')
-  // TODO: support Promise instance and function
-  if (!actions.every(f => isFuntion(f) || isPromise(f) )) {
+  if (!actions.every(f => isFuntion(f) || isPromiseInstance(f) )) {
     throw new Error('element in actions must be function or promise instance.')
   }
 }
